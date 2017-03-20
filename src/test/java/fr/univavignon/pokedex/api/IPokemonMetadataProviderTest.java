5106dbdf1a19e12a9abe4690a2a70962ebe4c309
Brugel Adrien <adrien.brugel@alumni.univ-avignon.fr>
2017-03-20 10:20:17 +0100

    test junit

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -0,0 +1,14 @@
+package fr.univavignon.pokedex.api;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IPokemonMetadataProviderTest {
+
+	@Test
+	public void test() {
+		assertEquals(1, 1);
+	}
+
+}
