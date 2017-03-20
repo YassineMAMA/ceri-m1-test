20568393c824dacf4484f0ad5a06ba1a90c9b3ab
Brugel Adrien <adrien.brugel@alumni.univ-avignon.fr>
2017-03-20 12:59:42 +0100

    Test mock

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -2,13 +2,40 @@ package fr.univavignon.pokedex.api;
 
 import static org.junit.gen5.api.Assertions.assertEquals;
 
+import org.junit.gen5.api.BeforeAll;
 import org.junit.gen5.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.mockito.runners.MockitoJUnitRunner;
 
 public class IPokemonMetadataProviderTest {
+	
+	@Mock Pokemon pokemonMock;
+	//@Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
+	
+	@BeforeAll
+    public void setUp() {
+		this.pokemonMock = Mockito.mock(Pokemon.class);
+		when(pokemonMock.getIndex()).thenReturn(0);
+		when(pokemonMock.getName()).thenReturn("Bulbizarre");
+		when(pokemonMock.getCp()).thenReturn(613);
+		when(pokemonMock.getHp()).thenReturn(64);
+		when(pokemonMock.getDust()).thenReturn(4);
+		when(pokemonMock.getAttack()).thenReturn(126);
+		when(pokemonMock.getDefense()).thenReturn(126);
+		when(pokemonMock.getStamina()).thenReturn(90);
+		when(pokemonMock.getIv()).thenReturn(new Double(56));
+    }
 
 	@Test
 	public void testInit() {
-		assertEquals(1, 1);
+		assertEquals(pokemonMock.getName(), "Bulbizarre");
+		//IPokemon a = new IPokemon();
 	}
 
 }
