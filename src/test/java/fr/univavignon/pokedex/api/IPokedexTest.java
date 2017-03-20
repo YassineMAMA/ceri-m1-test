1172c6845968052756ed934c159f1c0156ea6380
Brugel Adrien <adrien.brugel@alumni.univ-avignon.fr>
2017-03-20 10:39:07 +0100

    junit test

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -0,0 +1,14 @@
+package fr.univavignon.pokedex.api;
+
+import static org.junit.gen5.api.Assertions.assertEquals;
+
+import org.junit.gen5.api.Test;
+
+public class IPokedexTest {
+
+	@Test
+	public void test() {
+		assertEquals(2, 2);
+	}
+
+}
